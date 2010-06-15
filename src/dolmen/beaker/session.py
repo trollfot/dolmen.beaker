559698ac63df3ad7263ff6f36358fe40f52c5798
Souheil CHELFOUH <trollfot@gmail.com>
2010-06-15 17:04:50 +0200

    Current status of session.

--- src/dolmen/beaker/session.py
+++ src/dolmen/beaker/session.py
@@ -15,21 +15,12 @@ from zope.publisher.browser import TestRequest
 
 
 @grok.adapter(IHTTPRequest)
-@grok.provider(ISession)
 @grok.implementer(ISession)
 def ZopeSession(request):
     """Adapter factory from a Zope request to a beaker session
     """
-    print "EGEON"
-    return request.get(ENVIRON_KEY, None)
+    return request
 
-
-#class ZSession(grok.Adapter):
-#    grok.context(IHTTPRequest)
-#    grok.implements(ISession)
-#
-#    def __call__(self):
-#        import pdb; pdb.set_trace() 
 
 def initializeSession(request, environ_key='beaker.session'):
     """Create a new session and store it in the request.
